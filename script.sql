bcd2a5b3cbe796de23ecfa5d63bd2f6059e37bdf
wkktoria <127852624+wkktoria@users.noreply.github.com>
2023-10-30 16:59:03 +0100

    Create remaining queries

--- script.sql
+++ script.sql
@@ -1,7 +1,6 @@
--- Create a query to list out the event name and event data from the tblEvent table, with the most recent first.
+-- Create a query to list out the event name and event date from the tblEvent table, with the most recent first.
 SELECT
     EventName,
-    EventDetails,
     EventDate
 FROM
     tblEvent
@@ -26,3 +25,21 @@ SELECT
     *
 FROM
     categories;
+
+-- Write a query to show the first 5 events (in date order) from the tblEvent table. You should give the columns aliases (What and Details in this case); Even though you're sorting by the event date, it shouldn't be included in your results. 
+SELECT TOP 5
+    EventName AS What, EventDetails AS Details
+FROM tblEvent
+ORDER BY EventDate ASC;
+
+-- Create a query which uses two separate SELECT statements to show the first and last 2 events in date order from the tblEvent table. Redirect the output of this query to text, rather than to grid.
+SELECT TOP 2
+    EventName,
+    EventDate
+FROM tblEvent
+ORDER BY EventDate ASC;
+SELECT TOP 2
+    EventName,
+    EventDate
+FROM tblEvent
+ORDER BY EventDate DESC;
